0af1f28b487b1bb247d37eb7683e6419bab88a2f
Jianyu Zuo <amosqqmail1@qq.com>
2018-02-08 16:18:22 -0600

    Integrate JUnit & Mockito with Spring

---
+++ src/test/java/edu/tamu/amos/spring/firstspringproject/XmlSpringApplicationSpec.java
@@ -0,0 +1,28 @@
+package edu.tamu.amos.spring.firstspringproject;
+
+import edu.tamu.amos.spring.firstspringproject.level1.BinarySearchExample;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+// Load the Context
+// ContextConfiguration
+// Access wire @Autowired
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = "/XmlBeansTestOverride.xml")
+public class XmlSpringApplicationSpec {
+
+	@Autowired
+	BinarySearchExample binarySearchExample;
+
+	// Test Behaviors of BinarySearchExample
+	@Test
+	public void contextLoads() {
+	    assertEquals(10, binarySearchExample.binarySearch(new int[]{1,2,3}, 10));
+	}
+
+}
